7e5758ed9d1c1b267550bf878fc2feea65ce1cac
antonio.narkevich <antonio.narkevich@gmail.com>
2017-07-27 17:20:42 +0300

    Adds istanbul.

--- project.spec.js
+++ project.spec.js
@@ -1,7 +1,12 @@
 const assert = require('assert');
+const calculator = require('./calculator');
 
 describe('test project', () => {
   it('should have a passing test', () => {
       assert(1);
+  });
+
+  it('should sum 1 and 2', () => {
+      assert.equal(calculator.sum(1, 2), 1 + 2);
   });
 });
